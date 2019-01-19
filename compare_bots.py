9dec897172ce0042e9f36836663d2e7f4bfd5fc9
Martin Billinger <flkazemakase@gmail.com>
2019-01-19 08:34:23 +0100

    v18

--- compare_bots.py
+++ compare_bots.py
@@ -17,9 +17,9 @@ def run_game(bots, s):
 if __name__ == '__main__':
 
     bots = ['./target/release/my_bot',
-            #'./old_bots/v16 -c old_bots/v16.cfg.json',
-            #'./old_bots/v16 -c old_bots/v16.cfg.json',
-            './old_bots/v16 -c old_bots/v16.cfg.json']
+            #'./old_bots/v17 -c old_bots/v17.cfg.json',
+            #'./old_bots/v17 -c old_bots/v17.cfg.json',
+            './old_bots/v17 -c old_bots/v17.cfg.json']
 
     size = '32'
 
