2bb4f7c15c756f652deb6aa087ec1e590473f2ee
Martin Billinger <flkazemakase@gmail.com>
2019-01-22 21:30:40 +0100

    v23

--- compare_bots.py
+++ compare_bots.py
@@ -12,14 +12,14 @@ from sklearn.externals.joblib import Parallel, delayed
 def run_game(bots, s):
     x = subprocess.run(['./halite', '--no-replay', '--no-logs', '--results-as-json', '--width '+s, '--height '+s] + bots, stdout=subprocess.PIPE)
     result = json.loads(x.stdout)
-    return [result['stats'][str(i)]['score'] for i in range(len(bots))]
+    return [result['stats'][str(i)]['score'] for i in range(len(bots))], [result['stats'][str(i)]['rank'] for i in range(len(bots))]
 
 if __name__ == '__main__':
 
     bots = ['./target/release/my_bot',
-            #'./old_bots/v22 -c old_bots/v22.cfg.json',
-            #'./old_bots/v22 -c old_bots/v22.cfg.json',
-            './old_bots/v22 -c old_bots/v22.cfg.json']
+            #'./old_bots/v23 -c old_bots/v23.cfg.json',
+            #'./old_bots/v23 -c old_bots/v23.cfg.json',
+            './old_bots/v23 -c old_bots/v23.cfg.json']
 
     size = '32'
 
@@ -27,13 +27,16 @@ if __name__ == '__main__':
 
     scores_list = []
     n_wins = 0
+    ranks = [0] * len(bots)
     for k in ProgressBar()(range(n)):
         order = list(range(len(bots)))
         random.shuffle(order)
         reverse_order = [order.index(i) for i in range(len(order))]
 
-        s = run_game([bots[i] for i in order], size)
+        s, rs = run_game([bots[i] for i in order], size)
         n_wins += np.argmax(s) == order.index(0)
+        rank = rs[reverse_order[0]]
+        ranks[rank - 1] += 1
         scores_list.append((s[reverse_order[0]], np.mean([s[r] for r in reverse_order[1:]])))
 
         if (k + 1) % 10 == 0:
@@ -46,6 +49,7 @@ if __name__ == '__main__':
             print('mean:', np.mean(scores[:, 0] - scores[:, 1]))
             print('std:', np.std(scores[:, 0] - scores[:, 1]))
             print(n_wins, 'wins')
+            print(ranks)
 
     plt.hist(scores[:, 0] - scores[:, 1], int(np.sqrt(n)))
     plt.show()
