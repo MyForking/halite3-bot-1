383ff1da23e0aaced348ffc5fb5ec65f28bb8b74
Martin Billinger <flkazemakase@gmail.com>
2019-01-22 20:31:21 +0100

    fix ignoring opponents on own structures

--- src/movement_predictor.rs
+++ src/movement_predictor.rs
@@ -1,4 +1,3 @@
-use hlt::direction::Direction;
 use hlt::game::Game;
 use hlt::map_cell::Structure;
 use hlt::position::Position;
@@ -12,23 +11,41 @@ use hlt::position::Position;
     cargo: usize,
 }*/
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Threat {
+    Clear,
+    Reachable,
+    Occupied,
+}
+
+impl Threat {
+    fn set(&mut self, other: Self) {
+        use self::Threat::*;
+        match (*self, other) {
+            (Clear, o) => *self = o,
+            (Reachable, Occupied) => *self = Occupied,
+            _ => {}
+        }
+    }
+}
+
 pub struct MovementPredictor {
     //ships: HashMap<ShipId, ShipInfo>,
-    probs: Vec<Vec<f64>>,
+    threat_level: Vec<Vec<Threat>>,
 }
 
 impl MovementPredictor {
     pub fn new(w: usize, h: usize) -> Self {
         MovementPredictor {
             //ships: HashMap::new(),
-            probs: vec![vec![0.0; w]; h],
+            threat_level: vec![vec![Threat::Clear; w]; h],
         }
     }
 
     pub fn update_frame(&mut self, game: &Game) {
-        for row in &mut self.probs {
-            for p in row {
-                *p = 0.0;
+        for row in &mut self.threat_level {
+            for tl in row {
+                *tl = Threat::Clear;
             }
         }
 
@@ -50,28 +67,37 @@ impl MovementPredictor {
                 },
             );*/
 
-            self.probs[pos.y as usize][pos.x as usize] = match game.map.at_position(&pos).structure
-            {
-                // simply ignore enemy ships at my own structures
-                Structure::Dropoff(did) if game.dropoffs[&did].owner == game.my_id => 0.0,
-                Structure::Shipyard(pid) if pid == game.my_id => 0.0,
-                _ => 1.0,
-            };
+            for p in pos.get_surrounding_cardinals().into_iter().chain(std::iter::once(pos)) {
+                match game.map.at_position(&pos).structure
+                {
+                    // simply ignore enemy ships at my own structures
+                    Structure::Dropoff(did) if game.dropoffs[&did].owner == game.my_id => continue,
+                    Structure::Shipyard(pid) if pid == game.my_id => continue,
+                    _ => {},
+                };
+
+                if p == pos {
+                    self.threat_level[p.y as usize][p.x as usize] = Threat::Occupied;
+                } else {
+                    self.threat_level[p.y as usize][p.x as usize].set(Threat::Reachable);
+                }
+            }
         }
     }
 
     pub fn is_occupied(&self, pos: Position) -> bool {
         let pos = self.normalize(pos);
-        self.probs[pos.y as usize][pos.x as usize] > 0.99
+        self.threat_level[pos.y as usize][pos.x as usize] == Threat::Occupied
     }
 
     pub fn is_reachable(&self, pos: Position) -> bool {
-        Direction::get_all_options().into_iter().map(|d| pos.directional_offset(d)).any(|p| self.is_occupied(p))
+        let pos = self.normalize(pos);
+        self.threat_level[pos.y as usize][pos.x as usize] != Threat::Clear
     }
 
     pub fn normalize(&self, position: Position) -> Position {
-        let width = self.probs[0].len() as i32;
-        let height = self.probs.len() as i32;
+        let width = self.threat_level[0].len() as i32;
+        let height = self.threat_level.len() as i32;
         let x = ((position.x % width) + width) % width;
         let y = ((position.y % height) + height) % height;
         Position { x, y }
